907e5698b8527757e2df746b39f70a5bedb90093
M-Jay <1376031164@qq.com>
2021-01-10 20:53:29 +0800

    V0.1
    完成基本功能

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='user_manager',
+    version='0.1',
+    packages=['user_manager'],
+    url='https://blog.m-jay.cn/',
+    license='',
+    author='M - Jay',
+    author_email='1376031164@qq.com',
+    description='A Linux user operator'
+)
